42aa7cc9145a971f30a5fd0cc2e0843c6ed43694
paddywwoof <patrick@eldwick.org.uk>
2013-10-16 16:10:08 +0100

    bump to 1.4

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -36,10 +36,10 @@ autodoc_member_order = 'bysource'
 # order in the first place.
 
 # The short X.Y version.
-version = '1.3'
+version = '1.4'
 
 # The full version, including alpha/beta/rc tags.
-release = '1.3'
+release = '1.4'
 
 # General information about the project.
 project = u'pi3d'
